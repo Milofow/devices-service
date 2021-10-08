0c2d6020ac8b6673c6c5faade781f6c2616a92ac
Timmermans,Milo M.P.A <milo.timmermans@student.fontys.nl>
2021-10-08 11:01:11 +0200

    Started devices service project

---
+++ src/test/java/homeautomationdashboard/devicesservice/DevicesServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package homeautomationdashboard.devicesservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevicesServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
